97e59ee02794a433f341fc005b578726d538ad28
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-04-24 18:04:52 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className='relative'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
