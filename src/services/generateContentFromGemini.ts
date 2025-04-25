d9d84437aad3e6ca111c00a5db2f08880f9bb26a
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-04-25 20:59:32 +0530

    change end point

--- src/services/generateContentFromGemini.ts
+++ src/services/generateContentFromGemini.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 const SECRET_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+  
+const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${SECRET_KEY}`;
 
 const generateContentFromGemini = async (
   prompt: string | File
 ): Promise<string> => {
-  const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=${SECRET_KEY}`;
-
   try {
     if (prompt instanceof File) {
       const formData = new FormData();
